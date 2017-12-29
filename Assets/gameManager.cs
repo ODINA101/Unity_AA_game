2574a2415be37024b205def2ff2cb74b076898de
irakli samniashvili <iraklisamniashviliii@gmail.com>
2017-12-29 05:31:01 -0800

    first commit

---
+++ Assets/gameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    private bool gameHasEnded = false;
+    public rotator rotator;
+    public spawner spawner;
+
+    public Animator animator;
+
+
+
+
+    public void EndGame()
+    {
+        if (gameHasEnded)
+            return;
+        gameHasEnded = true;
+        rotator.enabled = false;
+        spawner.enabled = false;
+        Debug.Log("End game");
+        animator.SetTrigger("EndGame");
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/pin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pin : MonoBehaviour {
+
+    // Use this for initialization
+    public bool isPinned = false;
+    public float speed = 20f;
+
+    public Rigidbody2D rb;
+
+    private void Update()
+    {
+        if(!isPinned) { 
+        rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+        }
+    }
+    void OnTriggerEnter2D (Collider2D col)
+    { 
+        if(col.tag == "rotator")
+        {
+            transform.SetParent(col.transform);
+            col.GetComponent<rotator>().speed *= -1; // same col.GetComponent<rotator>().speed = col.GetComponent<rotator>().speed * -1 :)
+            isPinned = true;
+        }else if (col.tag == "Pin")
+        {
+
+            FindObjectOfType<gameManager>().EndGame();
+            //End game
+
+        }
+    }
+  
+}
